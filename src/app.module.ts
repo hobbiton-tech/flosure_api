8eb97e56eb892a1d181734ab58e30292a8b377e5
Jsilwembe <jsilwembe@gmail.com>
2020-05-04 16:33:35 +0200

    product fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,26 +9,26 @@ import { Product } from 'src/setups/entities/product.entity';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      // type: 'postgres',
-      // host: 'ec2-52-73-247-67.compute-1.amazonaws.com',
-      // port: 5432,
-      // username: 'qppyocwdpbilrq',
-      // password:
-      //   '831626c55d6f55a3d431280f046d0685000a844f54d24276647614dc999be92d',
-      // database: 'depqp2jclcrgp3',
-      // entities: [Class, Product],
-      // synchronize: true,
-      // autoLoadEntities: true,
-
       type: 'postgres',
-      host: 'localhost',
+      host: 'ec2-52-73-247-67.compute-1.amazonaws.com',
       port: 5432,
-      username: 'postgres',
-      password: 'joshua1234',
-      database: 'postgres',
+      username: 'qppyocwdpbilrq',
+      password:
+        '831626c55d6f55a3d431280f046d0685000a844f54d24276647614dc999be92d',
+      database: 'depqp2jclcrgp3',
       entities: [Class, Product],
       synchronize: true,
       autoLoadEntities: true,
+
+      // type: 'postgres',
+      // host: 'localhost',
+      // port: 5432,
+      // username: 'postgres',
+      // password: 'joshua1234',
+      // database: 'postgres',
+      // entities: [Class, Product],
+      // synchronize: true,
+      // autoLoadEntities: true,
     }),
     SetupsModule,
   ],
